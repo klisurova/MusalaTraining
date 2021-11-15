8819fae07c922b1cc3b08615539397c2367d9dad
klisurova <vbklisurova@gmail.com>
2021-11-15 17:52:04 +0200

    oop3

--- src/OOP/Computer.java
+++ src/OOP/Computer.java
@@ -10,28 +10,39 @@ public class Computer {
         this.procSpeed = procSpeed;
         this.stringArray = stringArray;
     }
-    public void setType(String type){
-        this.type=type;
+
+    public void setType(String type) {
+        if (type == null) {
+            System.out.println("No Computer type specified");
+        } else this.type = type;
     }
-    public String getType(){
+
+    public String getType() {
         return type;
     }
 
-    public void setProcSpeed(double procSpeed){
-        this.procSpeed = procSpeed;
+    public void setProcSpeed(double procSpeed) {
+        if (procSpeed < 1.1) {
+            System.out.println("Unidentified Processor Speed");
+        } else this.procSpeed = procSpeed;
     }
-    public double getProcSpeed(){
+
+    public double getProcSpeed() {
         return procSpeed;
     }
-    public void setStringArray(String[] stringArray){
-        this.stringArray = stringArray;
+
+    public void setStringArray(String[] stringArray) {
+        if (stringArray == null) {
+            System.out.println("The computer has no files stored");
+        } else this.stringArray = stringArray;
     }
-    public String[] getStringArray(){
+
+    public String[] getStringArray() {
         return stringArray;
     }
 
     public static void main(String[] args) {
-        ...
+
     }
 
 }
