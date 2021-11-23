f05ff040e0f2b4dcfb4304be5a4e9792f7e7c824
klisurova <94164039+klisurova@users.noreply.github.com>
2021-11-23 11:15:28 +0200

    Add files via upload

---
+++ src/OOP/savingsAccount.java
@@ -0,0 +1,60 @@
+package OOP;
+
+public class savingsAccount {
+
+    private double AnnualInterestRate;
+    private double SavingsBalance;
+    private int Number;
+
+    public savingsAccount(double AnnualInterestRate) {
+        this.AnnualInterestRate = AnnualInterestRate;
+    }
+
+    public savingsAccount(double SavingsBalance, int Number) {
+        this.SavingsBalance = SavingsBalance;
+        this.Number = Number;
+    }
+
+    public savingsAccount(double AnnualInterestRate, double SavingsBalance, int Number) {
+        this.AnnualInterestRate = AnnualInterestRate;
+        this.SavingsBalance = SavingsBalance;
+        this.Number = Number;
+    }
+
+    public void setAnnualInterestRate(double AnnualInterestRate) {
+        this.AnnualInterestRate = AnnualInterestRate;
+    }
+
+    public double getAnnualInterestRate() {
+        return AnnualInterestRate;
+    }
+
+    public void setSavingsBalance(double SavingBalance) {
+        this.SavingsBalance = SavingBalance;
+    }
+
+    public double getSavingsBalance() {
+        return SavingsBalance;
+    }
+
+    public void setNumber(int Number) {
+        this.Number = Number;
+    }
+
+    public int getNumber() {
+        return Number;
+    }
+
+
+    public String toString() {
+        System.out.println(this.SavingsBalance);
+        return null;
+    }
+
+
+    public static void main(String[] args) {
+
+    }
+
+
+}
