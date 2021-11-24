61daae330b818cbb28362cad7c2567017bfcf61d
klisurova <vbklisurova@gmail.com>
2021-11-24 13:31:13 +0200

    savingsAccount

--- src/OOP/savingsAccount.java
+++ src/OOP/savingsAccount.java
@@ -2,85 +2,83 @@ package OOP;
 
 public class savingsAccount {
 
-    private double AnnualInterestRate;
-    private double SavingsBalance;
-    private int Number;
+    private static double annualInterestRate = 2.5;
+    private double savingsBalance;
+    private int number;
 
-    public savingsAccount(double AnnualInterestRate) {
-        this.AnnualInterestRate = AnnualInterestRate;
+    // public savingsAccount(double AnnualInterestRate) {
+    //   this.AnnualInterestRate = AnnualInterestRate;
+    // }
+
+    public savingsAccount(double savingsBalance) {
+        this.savingsBalance = savingsBalance;
+        annualInterestRate = 2.5;
     }
 
-    public savingsAccount(double SavingsBalance, int Number) {
-        this.SavingsBalance = SavingsBalance;
-        this.Number = Number;
+    //public savingsAccount(savingsAccount account) {
+      //  savingsBalance = account.savingsBalance;
+        //annualInterestRate = account.getannualInterestRate();
+    //}
+
+    public double getannualInterestRate() {
+        return annualInterestRate;
     }
 
-    public savingsAccount(double AnnualInterestRate, double SavingsBalance, int Number) {
-        this.AnnualInterestRate = AnnualInterestRate;
-        this.SavingsBalance = SavingsBalance;
-        this.Number = Number;
+    public void setsavingsBalance(double savingBalance) {
+        this.savingsBalance = savingBalance;
     }
 
-    public void setAnnualInterestRate(double AnnualInterestRate) {
-        this.AnnualInterestRate = AnnualInterestRate;
+    public double getsavingsBalance() {
+        return savingsBalance;
     }
 
-    public double getAnnualInterestRate() {
-        return AnnualInterestRate;
-    }
-
-    public void setSavingsBalance(double SavingBalance) {
-        this.SavingsBalance = SavingBalance;
-    }
-
-    public double getSavingsBalance() {
-        return SavingsBalance;
-    }
-
-    public void setNumber(int Number) {
-        this.Number = Number;
+    public void setnumber(int number) {
+        this.number = number;
     }
 
     public int getNumber() {
-        return Number;
+        return number;
     }
 
     double calculateMonthlyInterest;
+
     public double calculateMonthlyInterest() {
-        calculateMonthlyInterest = (this.SavingsBalance * this.AnnualInterestRate) / 12;
-        return (calculateMonthlyInterest + this.SavingsBalance);
+        calculateMonthlyInterest = (this.savingsBalance * annualInterestRate) / 12;
+        return (calculateMonthlyInterest + this.savingsBalance);
     }
 
     public String toString() {
-        System.out.println(this.SavingsBalance);
+        System.out.println(this.savingsBalance);
         System.out.println(calculateMonthlyInterest);
         return null;
     }
 
     double modifyInterestRate;
 
-    //public static void setModifyInterestRate(double modifyInterestRate) {
-
-    public void isGreater(){
-
-
-    };
+    public static void modifyInterestRate(double modifyInterestRate) {
+        annualInterestRate = modifyInterestRate;
+    }
 
 
+    public boolean isGreater(savingsAccount account) {
+        return this.savingsBalance > account.savingsBalance;
+    }
 
     public static void main(String[] args) {
 
-        savingsAccount account1 = new savingsAccount(1232.5, 3);
-        savingsAccount account2 = new savingsAccount(1452.8, 4);
-        if (account1.SavingsBalance == account2.SavingsBalance) {
-            System.out.println("Account1 and Account2 have equal saving balances");
-        } else System.out.println("Account1 and Account2 haven't equal saving balances");
+        savingsAccount account1 = new savingsAccount(1232.5);
+        savingsAccount account2 = new savingsAccount(1452.8);
+        if (account1.isGreater(account2)) {
+            System.out.println("Account1 savings balance is greater than Account2");
+        } else System.out.println("Account1 savings balance is NOT greater than Account2");
 
         System.out.println();
-        savingsAccount account3 = new savingsAccount(1232.5,5);
-        if (account1.SavingsBalance == account3.SavingsBalance) {
+        savingsAccount account3 = new savingsAccount(1232.5);
+        if (account1.savingsBalance == account3.savingsBalance) {
             System.out.println("Account1 and Account3 have equal saving balances");
         } else System.out.println("Account1 and Account3 haven't equal saving balances");
+
+
     }
 
 }
